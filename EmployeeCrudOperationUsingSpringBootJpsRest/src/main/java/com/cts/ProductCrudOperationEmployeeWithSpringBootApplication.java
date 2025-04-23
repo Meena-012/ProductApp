02c17cf1d894ae58050fe7150521a53ed9b01d1a
meenakshi <ms12032004@gmail.com>
2025-04-23 10:23:59 +0530

    first commit to github

--- EmployeeCrudOperationUsingSpringBootJpsRest/src/main/java/com/cts/ProductCrudOperationEmployeeWithSpringBootApplication.java
+++ EmployeeCrudOperationUsingSpringBootJpsRest/src/main/java/com/cts/ProductCrudOperationEmployeeWithSpringBootApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class ProductCrudOperationEmployeeWithSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCrudOperationEmployeeWithSpringBootApplication.class, args);
-	
+	System.out.println("checking");
 	}
 
 }
